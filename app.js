048907b0179739ece2d9db2b2ba25e2281a3cfe8
mainuddin sani <main12sani@gmail.com>
2021-07-24 20:39:46 +0600

    pin generate app done

--- app.js
+++ app.js
@@ -2,29 +2,53 @@ function getInputName(input_id) {
     return document.getElementById(input_id);
 }
 
-function getValue() {
-    return getInputName('calculate_display');
-}
-function printValue(num) {
-    return getInputName('calculate_display').value = num;
+
+
+function generateValue() {
+    let random = Math.floor(Math.random() * 10000);
+    if (random.toString().length === 4) {
+        return random;
+    } else {
+        console.log(generateValue());
+        return generateValue();
+    }
 }
 
 function pinGenerate() {
-    return getInputName('Pin_matcher').value = Math.floor(Math.random() * 10000);
-}
-
-let buttons = document.querySelectorAll('.button');
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', function (e) {
-        let clickedButtonValue = e.target.innerText;
-        let calculate_num = getInputName('calculate_display').value += clickedButtonValue;
-     });
+     // display set value
+     let valueSet = getInputName('Pin_matcher').value = generateValue();
+     console.log(valueSet);
     
 }
 
-function pinMatch() {
-    let get_Calculate_number = clickedButtonValue;
-    console.log(get_Calculate_number);
-}
+let buttons_container = getInputName('buttons-container');
+buttons_container.addEventListener('click', function (e) {
+    let digit = e.target.innerText
+    if (isNaN(digit)) {
+        // backspace event handler 
+        // clear event handler
+    } else {
+        getInputName('calculate_display').value = getInputName('calculate_display').value + digit;
+    }
+})
 
-pinMatch();
+function verifyButton() {
+    let pin = getInputName('Pin_matcher').value;
+    let calculate_pin = getInputName('calculate_display').value;
+    if (pin == calculate_pin) {
+        let verify_massage = getInputName('PinMatched');
+        verify_massage.style.display = 'block';
+    } else {
+        let verify_massage = getInputName('dontMatch');
+        verify_massage.style.display = 'block';
+    }
+    
+}
+// for (let i = 0; i < buttons.length; i++) {
+//     buttons[i].addEventListener('click', function (e) {
+//         let clickedButtonValue = e.target.innerText;
+//         let calculate_num = getInputName('calculate_display').value += clickedButtonValue;
+//      });
+// }
+
+
